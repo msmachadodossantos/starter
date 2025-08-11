8d6d0be0c9aff8d57dd79de3b30cfc41555aac15
Machado dos Santos <29000264+msmachadodossantos@users.noreply.github.com>
2025-08-11 15:23:13 +0100

    Create pj-navigation.js

---
+++ public/asset/js/pj-navigation.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview This script adjusts the `padding-top` and `padding-bottom`
+ * of the `<body>` element based on the current height of fixed navigation bars.
+ *
+ * It waits for the DOM content to be fully loaded before executing.
+ * This prevents the script from running on elements that haven't been rendered yet.
+ *
+ * @listens DOMContentLoaded
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+
+  const topNavigation = document.querySelector(".pj-navigation.fixed-top");
+  const bottomNavigation = document.querySelector(
+    ".pj-navigation.fixed-bottom",
+  );
+
+  let topHeight = 0;
+  let bottomHeight = 0;
+
+  if (topNavigation) {
+    topHeight = topNavigation.offsetHeight;
+  }
+
+  if (bottomNavigation) {
+    bottomHeight = bottomNavigation.offsetHeight;
+  }
+
+  body.style.paddingTop = `${topHeight}px`;
+  body.style.paddingBottom = `${bottomHeight}px`;
+});
